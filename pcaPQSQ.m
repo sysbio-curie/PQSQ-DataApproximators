2412adc786c7ca64087228feb425169b41ee2511
Evgeny Mirkes <em322@leicester.ac.uk>
2016-03-16 15:04:41 +0000

    Correction of firstPrincipalComponentPQSQ
    There are several changes:
    1 I transform V from row vector into column vector. It is standard for
    all pca methods.
    2. I slightly improve performance of calculation

--- pcaPQSQ.m
+++ pcaPQSQ.m
@@ -1,82 +1,102 @@
-function [V,U,C] = pcaPQSQ(x, ncomp, varargin)
+function [V,U,C,D] = pcaPQSQ(x, ncomp, varargin)
+%pcaPQSQ calculates the first ncomp principal components for dataset in
+%matrix x.
+%   Input attributes
+%       x is n-by-m matrix with n observations and m attributes.
+%       ncomp i a number of principal components to calculate
+%       Other input features are optional and can be specified by Name,
+%       Value pairs or by Value only:
+%       'verbose', 1 serves to switch on detailed output form all functions.
+%               Mostly used for debuging.
+%       'intervals', intervals serves to specify user defined intervals.
+%               intervals is matrix each row of which corresponds to one
+%               dimension. In each row the first element must be zero.
+%   Output arguments
+%       V is m-by-ncomp matrix of ncomp columns each of which is the
+%           principal component.
+%       U is n by ncomp matrix of ncomp columns each of which contains
+%           lenths of projection of data point (row) onto corresponding
+%           principal component.
+%       C is 1-by-m the vector of PQSQ mean (coordinates of central point).
+%       D is 1-by-ncomp vctor of values of fraction of explained energy.
 
-verbose=0;
-intervals = defineIntervals(x,5);
-useJavaImplementation = 0;
+    verbose=0;
+    intervals = defineIntervals(x,5);
+    useJavaImplementation = 0;
 
-for i=1:length(varargin)
-    if strcmp(varargin{i},'verbose')
-        verbose = varargin{i+1};
+    for i=1:length(varargin)
+        if strcmp(varargin{i},'verbose')
+            verbose = varargin{i+1};
+        end
+        if strcmp(varargin{i},'intervals')
+            intervals = varargin{i+1};
+        end
+        if strcmp(varargin{i},'javacode')
+            useJavaImplementation = 1;
+        end
+        if strcmp(varargin{i},'potential')
+            potential_function_handle = varargin{i+1};
+        end
     end
-    if strcmp(varargin{i},'intervals')
-        intervals = varargin{i+1};
-    end
-    if strcmp(varargin{i},'javacode')
-        useJavaImplementation = 1;
-    end
-    if strcmp(varargin{i},'potential')
-        potential_function_handle = varargin{i+1};
-    end
-end
 
-if ~useJavaImplementation
+    if ~useJavaImplementation
 
 
-%calculate central point
-C = PQSQ_Mean(x,intervals,potential_function_handle);
+        %calculate central point
+        C = PQSQ_Mean(x,intervals,potential_function_handle);
 
-% initiate xwork like x-C
-xwork = bsxfun(@minus,x,C);
+        % initiate xwork like x-C
+        xwork = bsxfun(@minus,x,C);
 
-for i=1:ncomp
-    if verbose
-        display(sprintf('Component %i',i));
-    end
-    
-    %Calculate one component    
-    [Vi,Ui] = firstPrincipalComponentPQSQ(xwork, intervals, potential_function_handle, varargin{:});
-    
-    V(i,:) = Vi;
-    U(:,i) = Ui;
-    
-    %%%%
-    % You might want to use PQSQ-based projections: this should increase precision?
-    %%%%%
-     s2 = sqrt(sum(Vi.^2));
-     Vi = Vi/s2;
-     s1 = xwork*Vi';
-     xwork = xwork - s1*Vi;
+        for i=1:ncomp
+            if verbose
+                display(sprintf('Component %i',i));
+            end
 
-    
-end
+            %Calculate one component
+            [Vi,Ui] = firstPrincipalComponentPQSQ(xwork, intervals, potential_function_handle, varargin{:});
 
-else % java implementation
-    javaclasspath({'VDAOEngine.jar'});
-    pca = vdaoengine.analysis.PCAMethodPQSQ;
-    pca.setData(x);
-    if strcmp(potential_function_handle,'L1')
-        pca.PQSQpotential = vdaoengine.analysis.PQSQPotential.getTrimmedLinearPQSQPotential(x);
+            V(i,:) = Vi;
+            U(:,i) = Ui;
+
+            %%%%
+            % You might want to use PQSQ-based projections: this should increase precision?
+            %%%%%
+            s2 = sqrt(sum(Vi.^2));
+            Vi = Vi/s2;
+            s1 = xwork*Vi;
+            xwork = xwork - s1*Vi';
+
+
+        end
+
+    else % java implementation
+        javaclasspath({'VDAOEngine.jar'});
+        pca = vdaoengine.analysis.PCAMethodPQSQ;
+        pca.setData(x);
+        if strcmp(potential_function_handle,'L1')
+            pca.PQSQpotential = vdaoengine.analysis.PQSQPotential.getTrimmedLinearPQSQPotential(x);
+        end
+        if strcmp(potential_function_handle,'L2')
+            pca.PQSQpotential = vdaoengine.analysis.PQSQPotential.getTrimmedQuadraticPQSQPotential(x);
+        end
+        if strcmp(potential_function_handle,'LSQRT')
+            pca.PQSQpotential = vdaoengine.analysis.PQSQPotential.getTrimmedSqrtPQSQPotential(x);
+        end
+
+        pca.PQSQpotential.numberOfIntervals = size(intervals,2);
+        pca.PQSQpotential.intervals = intervals;
+        pca.PQSQpotential.computeABCoefficients();
+
+        pca.verboseMode = 0;
+        pca.calcBasis(ncomp);
+        C = pca.getBasis().a0;
+        C = C';
+        U = pca.pointProjections;
+        U = U';
+        U = U(:,1:ncomp);
+        V = pca.getBasis().basis;
     end
-    if strcmp(potential_function_handle,'L2')
-        pca.PQSQpotential = vdaoengine.analysis.PQSQPotential.getTrimmedQuadraticPQSQPotential(x);
-    end
-    if strcmp(potential_function_handle,'LSQRT')
-        pca.PQSQpotential = vdaoengine.analysis.PQSQPotential.getTrimmedSqrtPQSQPotential(x);
-    end
-    
-    pca.PQSQpotential.numberOfIntervals = size(intervals,2);
-    pca.PQSQpotential.intervals = intervals;
-    pca.PQSQpotential.computeABCoefficients();
-    
-    pca.verboseMode = 0;
-    pca.calcBasis(ncomp);
-    C = pca.getBasis().a0;
-    C = C';
-    U = pca.pointProjections;
-    U = U';
-    U = U(:,1:ncomp);
-    V = pca.getBasis().basis;
-end
 
 end
 
--- splitPointIntervalsFast1.m
+++ splitPointIntervalsFast1.m
@@ -1,3 +1,3 @@
 function inds = splitPointIntervalsFast1(x,intervals)
 
-[A, inds] = histc(abs(x),intervals);
+[~, inds] = histc(abs(x),intervals);
